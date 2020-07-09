239874a2cde0cb02a9c8217a537a1bfc7e50d88d
TanawatDEVz <takissnit@hotmail.com>
2020-07-09 21:54:50 +0700

    Add middleware

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 // ExampleDataBody ...
@@ -16,11 +17,35 @@ type ExampleDataBody struct {
 
 func main() {
 	e := echo.New()
-	e.GET("/:id", ExampleGet)
-	e.POST("/json-body", ExamplePostJSONBody)
-	e.POST("/xml-body", ExamplePostXMLBody)
+
+	// Root level middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Group level middleware
+	g := e.Group("/body")
+	g.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == "tanawat" && password == "198777" {
+			return true, nil
+		}
+		return false, nil
+	}))
+
+	// Route level middleware
+	track := func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			println("request to root route")
+			return next(c)
+		}
+	}
+
+	e.GET("/:id", ExampleGet, track)
 	e.POST("/form-urlencoded", ExamplePostForm)
 	e.POST("/multipart-form-data", ExamplePostMultiPart)
+	g.POST("/json", ExamplePostJSONBody)
+	g.POST("/xml", ExamplePostXMLBody)
+	g.POST("/form-data", ExamplePostFormDataBody)
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
@@ -47,6 +72,15 @@ func ExamplePostXMLBody(c echo.Context) error {
 		return err
 	}
 	return c.XML(http.StatusCreated, d)
+}
+
+// ExamplePostFormDataBody ...
+func ExamplePostFormDataBody(c echo.Context) error {
+	d := new(ExampleDataBody)
+	if err := c.Bind(d); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusCreated, d)
 }
 
 // ExamplePostForm ...
